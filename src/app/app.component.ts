3e9912cc200a7d45e4279b1d5c1c8aa6b61a465f
HASSANE WANKOYE <hassane.wankoye@otimbi.com>
2019-12-17 00:46:23 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  posts = [
+    {
+      title: 'Machine à laver',
+      content: 'juste un test',
+      loveIts: 2,
+      created_at: Date()
+    },
+    {
+      title: 'Machine à laver 1',
+      content: 'juste un test',
+      loveIts: -2,
+      created_at: Date()
+    },
+    {
+      title: 'Machine à laver 2',
+      content: 'juste un test',
+      loveIts: 0,
+      created_at: Date()
+    },
+    {
+      title: 'Machine à laver 3',
+      content: 'juste un test',
+      loveIts: 0,
+      created_at: Date()
+    },
+  ];
 }
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() created_at: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  addLike(){
+    this.loveIts += 1;
+  }
+  removeLike(){
+    this.loveIts -= 1;
+  }
+  getColor() {
+    if (this.loveIts > 0) {
+      return 'green';
+    } else if (this.loveIts < 0) {
+      return 'red';
+    }
+  }
+
+}
